708b5bf2072f5457f5ddedb5ab9696c16cc649f4
fire <1542931780@qq.com>
2017-04-09 16:53:42 +0800

    邮箱验证

--- Caipu/pages/feedback/feedback.js
+++ Caipu/pages/feedback/feedback.js
@@ -2,7 +2,7 @@ var app = getApp()
 var common = require('../../utils/common.js').userInfo()
 Page({
   data: {
-    
+
   },
   onLoad: function (options) {
     // 生命周期函数--监听页面加载
@@ -11,30 +11,41 @@ Page({
     var that = this;
     var emailinfo = e.detail.value.mail;
     var describeinfo = e.detail.value.describe;
-    console.log("获取email"+emailinfo);
-    console.log("获取描述"+describeinfo);
+    console.log("获取email" + emailinfo);
+    console.log("获取描述" + describeinfo);
+    //判断是否为邮箱的正则表达式
+    var usableEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
 
-    wx.request({
-      url: app.globalData.globalUrl + "insertfaq",
-      method: 'POST',
-      header: {
-        'content-type': 'application/json'
-      },
-      data: {
-        "openid": common.openid,
-        "email": emailinfo,
-        "description": escribeinfo
-      },
+    // console.log(usableEmail.test(emailinfo));
+    if (usableEmail.test(emailinfo)) {
+      wx.request({
+        url: app.globalData.globalUrl + "insertfaq",
+        method: 'POST',
+        header: {
+          'content-type': 'application/json'
+        },
+        data: {
+          "openid": common.openid,
+          "email": emailinfo,
+          "description": describeinfo
+        },
 
-      success: function (res) {
+        success: function (res) {
 
-        console.log("当前网址反馈成功");
-      },
-      fail: function (error) {
-        console.log(error);
+          console.log("当前网址反馈成功");
+        },
+        fail: function (error) {
+          console.log(error);
 
-      }
-    })
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '请输入正确的邮箱地址',
+        duration: 2000
+      })
+    }
+
 
   }
 })
